b3ae61206b90fee51645968794f69266ebacdbd8
Brianna Rodriguez <Brianna.Rodriguez@s.capregboces.org>
2024-02-28 10:15:56 -0500

    not done

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 10;
+
+            for ( int i = 0; i < maxNumber; i++)
+            {
+                Console.WriteLine(
+            }
+
+        }
+    }
+}
