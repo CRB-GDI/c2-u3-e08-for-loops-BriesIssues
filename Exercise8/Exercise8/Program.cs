c779cfc10220bd6a13f39afdd2b739ac36471760
Brianna Rodriguez <Brianna.Rodriguez@s.capregboces.org>
2024-02-29 09:48:29 -0500

    Ready for grading

--- Exercise8/Exercise8/Program.cs
+++ Exercise8/Exercise8/Program.cs
@@ -4,13 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" How Many even numbers");
+            Console.WriteLine(" How Many even numbers?");
             int maxNumber = int.Parse (Console.ReadLine());
             int loopCounter;
             // prien even numbers up to requested max
             for (loopCounter = 0; loopCounter < maxNumber; loopCounter +=2 )
             {
                 Console.WriteLine(loopCounter);
+            }
+
+            //prompt 2
+            Console.WriteLine("How wide?");
+            int maxwith = int.Parse(Console.ReadLine());
+            for (int i = 0; i < maxwith; i++)
+            {
+                //if user enters 5, should print *****
+                Console.Write("*");
             }
         }
     }
